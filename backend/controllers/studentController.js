6623374397ced2ae948e830af76591f2e45933d4
Sreeram <sreeram.asam95@gmail.com>
2026-07-11 15:09:00 +0530

    create student controller

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,19 @@
+const pool = require('../db');
+
+// @desc Get all students belonging to a specific class
+// @route GET /api/students/:classId
+// @access Private (JWT Required)
+const getStudentsByClass = async (req, res) => {
+    const { classId } = req.params;
+
+    try {
+        const result = await pool.query('SELECT * FROM students WHERE class_id = $1', [classId]);
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.error('Error fetching students:', err.message);
+        res.status(500).json({message: 'Server error fetching students' });
+    }
+};
+
+module.exports = { getStudentsByClass };
